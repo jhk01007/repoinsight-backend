87d2bd1cc3d858ca52794f1e8d6afd6b81286283
jhk01007 <jhk001007@naver.com>
2025-11-13 21:18:52 +0900

    chore: 디렉토리 구조 변경

--- llm/search/chain/GithubSearchQueryChain.py
+++ llm/search/chain/GithubSearchQueryChain.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
-from llm.search.git_languages import validate_support
-from llm.search.WebGithubDocsSplitter import WebGithubDocsSplitter
+from llm.search.util.git_languages import validate_support
+from llm.search.util.WebGithubDocsSplitter import WebGithubDocsSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
