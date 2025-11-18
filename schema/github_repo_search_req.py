76bea620781e7d9ad6444f368f45d6b12d1851d6
jhk01007 <jhk001007@naver.com>
2025-11-18 14:06:27 +0900

    feat(github_repo_search_req.py): 키워드 길이 검증 추가

--- schema/github_repo_search_req.py
+++ schema/github_repo_search_req.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class GithubRepoSearchReq(BaseModel):
-    keyword: str # 리포지토리 키워드
+    keyword: str = Field(max_length=50, description="키워드는 50자를 넘을 수 없습니다.") # 리포지토리 키워드
     languages: list[str] = []# 리포지토리 언어 목록
